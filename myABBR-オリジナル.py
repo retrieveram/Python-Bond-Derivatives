ddd2912403fe1563e441f9ddcfc633a46fd23219
retrieveram <151920356+retrieveram@users.noreply.github.com>
2025-11-22 11:04:30 +0900

    Add files via upload

---
+++ myABBR-オリジナル.py
@@ -0,0 +1,124 @@
+#---- A. ライブラリーインポート ----
+import QuantLib          as ql
+import datetime          as dt 
+import pandas            as pd
+import numpy             as np 
+import matplotlib.pyplot as plt 
+import matplotlib.ticker as mtick
+import warnings          #警告の非表示用(pandas ilocで止める)
+from functools import singledispatch   #関数オーバーロード用
+
+#---- B. matplotlib初期設定  ----
+#     日本語フォントとサイズ、グラフサイズ
+plt.rcParams.update({"font.family"   :"MS Gothic",
+                     "font.size"     :9,
+                     "figure.figsize":[4.5,2.5]})
+
+#---- C. numpy初期設定 5桁表示と配列短縮形 ----
+np.set_printoptions(precision=5,suppress=True) 
+def nSetP():                     # %5桁表示設定
+  np.set_printoptions(formatter={'float':'{:.5%}'.format}) 
+def nSetF():                     # float5桁表示設定
+  np.set_printoptions(precision=5,suppress=True) 
+def nA(LIST):        return np.array(LIST)
+
+#---- D. pandasスタイル書式用 辞書型変数 ----
+fmtSCF = {'amount':  '{:,.2f}', 'atmFWD':  '{:.6%}', 'coupon':  '{:.6%}',
+          'days':     '{:.0f}', 'DF':      '{:.8f}', 'nominal':'{:,.2f}',
+          'NPV':     '{:,.2f}', 'matYR':  '{:,.4f}', 'parRT':   '{:.6%}',
+          'rate':     '{:.6%}', 'shftRT':  '{:.6%}', 'spread':  '{:.3%}',
+          'zeroRT':   '{:.6%}'}
+fmtFUT = {'accruAMT':'{:,.4f}', 'amount': '{:,.4f}', 'BPV':     '{:.4f}',
+          'CF':       '{:.5f}', 'cleanPRC':'{:.4f}', 'coupon':  '{:.4%}',
+          'dirtyPRC': '{:.4f}', 'gBASIS':  '{:.4f}', 'yield':   '{:.4f}'}
+
+#---- E. 日付関連メソッドの短縮形 ----
+# Days, Weeks, Months, Years
+DD = ql.Days ;  WW = ql.Weeks ;  MM = ql.Months ;  YY = ql.Years
+# japan日付
+def jDT(yyyy,mm,dd): return ql.Date(dd,mm,yyyy)
+# datetimeクラスからQL Date
+def dDT(dateTIME):   return ql.Date().from_date(dateTIME)
+# iso日付
+def iDT(isoDT):      return ql.Date(isoDT, '%Y-%m-%d')
+# 曜日
+def dayOfWeek(Date): return Date.to_date().strftime('%a')
+# SettingクラスevaluationDate設定
+def setEvDT(evaluationDT):  
+  ql.Settings.instance().evaluationDate = evaluationDT
+
+# Period 3種類の短縮形 (2番目はタプルが引数)
+@singledispatch
+def pD(pdSTR: str): return ql.Period(pdSTR)  # Period('3M')
+@pD.register(tuple)
+def _(nnUNT):       return ql.Period(*nnUNT) # Period((3,MM))
+@pD.register(int)
+def _(FRQ):         return ql.Period(FRQ)    # Period(freqQ) 
+
+
+#---- F. 短縮形リスト ----
+# Calendar
+calJP   =  ql.Japan()
+calEU   =  ql.TARGET()
+calUSf  =  ql.UnitedStates(ql.UnitedStates.FederalReserve)
+calUSg  =  ql.UnitedStates(ql.UnitedStates.GovernmentBond)
+calUSs  =  ql.UnitedStates(ql.UnitedStates.SOFR)
+calWK   =  ql.WeekendsOnly()
+calNL   =  ql.NullCalendar()
+# DayCounter
+dcA365  =  ql.Actual365Fixed()
+dcA360  =  ql.Actual360()       # includeLastDay=false
+dcA360t =  ql.Actual360(True)   # for CDS
+dc30    =  ql.Thirty360(ql.Thirty360.BondBasis)
+dc30e   =  ql.Thirty360(ql.Thirty360.European)
+dcAA    =  ql.ActualActual(ql.ActualActual.ISDA)
+dcAAb   =  ql.ActualActual(ql.ActualActual.Bond)
+# T + Business days (settle days)
+Tp0     =  0
+Tp1     =  1
+Tp2     =  2
+Tp3     =  3
+# freqency
+freqA   =  ql.Annual                  # 1
+freqSA  =  ql.Semiannual              # 2
+freqQ   =  ql.Quarterly               # 4
+freqD   =  ql.Daily                   # 365
+# tenor (period version for freq)
+pdFreqA =  ql.Period(ql.Annual)       # 1Y
+pdFreqSA=  ql.Period(ql.Semiannual)   # 6M
+pdFreqQ =  ql.Period(ql.Quarterly)    # 3M
+pdFreqD =  ql.Period(ql.Daily)        # 1D
+# convension
+mFLLW   =  ql.ModifiedFollowing
+FLLW    =  ql.Following
+unADJ   =  ql.Unadjusted
+# date generation
+dtGENb  =  ql.DateGeneration.Backward
+dtGENf  =  ql.DateGeneration.Forward
+dtGEN20 =  ql.DateGeneration.TwentiethIMM
+dtGENc  =  ql.DateGeneration.CDS
+dtGEN15 =  ql.DateGeneration.CDS2015
+# end of month
+EoMf    =  False
+EoMt    =  True
+# compound
+cmpdCMP =  ql.Compounded
+cmpdCNT =  ql.Continuous
+cmpdSPL =  ql.Simple
+# currency
+jpyFX   =  ql.JPYCurrency()
+usdFX   =  ql.USDCurrency()
+eurFX   =  ql.EURCurrency()
+# CDS : recovery rate / coupon
+rcvRTz  = 0.0     # zero
+rcvRTj  = 0.35    # Japan
+rcvRTu  = 0.40    # US
+rcvRTs  = 0.20    # subordinate
+cpn025  = 0.0025
+cpn100  = 0.01
+cpn500  = 0.05
+bP      = ql.Protection.Buyer  # 0
+sP      = ql.Protection.Seller # 1
+# others
+parPR   = 100.0
+parAMT  = 100.0
---
+++ myUtilｰオリジナル.py
@@ -0,0 +1,248 @@
+from myABBR import *
+from scipy.stats import norm
+  
+##### ショートカット #####
+# シンプルクォート
+def sqHDL(xx):   
+  '''sqHDL(xx)=ql.QuoteHandle(ql.SimpleQuote(xx))''' 
+  return ql.QuoteHandle(ql.SimpleQuote(xx))
+
+# フラットフォワード ** OBJとTSHの2つを戻す点に注意  **
+def ffTSH(settleDT, rate, dc=dcA365, cmpd=2, freq=1):   
+  '''ffTSH(settleDT,rate,dc=dcA365,cmpd=2,freq=1) 
+                                    = ffCrvOBJ,ql.YTS(ql.FlatForward(...))
+     cmpd=2:Continuous, 1:Compounded freq=1:Annual 2:Semiannual''' 
+  ffCrvOBJ = ql.FlatForward(settleDT, rate, dc, cmpd, freq)
+  ffCrvOBJ.enableExtrapolation()
+  return (ffCrvOBJ, ql.YieldTermStructureHandle(ffCrvOBJ))
+
+# ブラックコンスタントボラTSハンドル
+def bVolTSH(tradeDT, vol, cal=calWK, dc=dcA365):   
+  '''bVolTSH(tradeDT, vol, cal=calWK, dc=dcA365)
+                                    =ql.BlackVolTSH(ql.BlackConstantVol(...))''' 
+  return ql.BlackVolTermStructureHandle(
+                    ql.BlackConstantVol(tradeDT, cal, vol, dc))
+
+##### 各種カーブオブジェクト作成 #####
+# SOFRカーブ
+def makeSofrCurve(crvDATA):
+    '''makeSofrCurve(crvDATA)->[sofrIX,sfCrvOBJ,sfCrvHDL,sfParRT]'''      
+  # 1.指標金利オブジェクトと初期値設定
+    sfCrvHDL = ql.RelinkableYieldTermStructureHandle()  
+    sofrIX = ql.Sofr(sfCrvHDL)
+  # 2. HelperとSOFRカーブオブジェクト
+    cHelper, sfParRT = [], []
+    for knd, tnr, rt in crvDATA:
+        if knd == 'depo':
+            if ql.Period(tnr).length() == 1:
+                cHelper.append(ql.DepositRateHelper(sqHDL(rt/100),sofrIX)) 
+        if knd == 'swap': cHelper.append(
+            ql.OISRateHelper(Tp2, ql.Period(tnr),sqHDL(rt/100),sofrIX))
+        sfParRT.append(rt/100)                             # パーレート用リスト
+    sfCrvOBJ = ql.PiecewiseLogLinearDiscount(Tp0, calUSs, cHelper, dcA360)
+    sfCrvHDL.linkTo(sfCrvOBJ) ; sfCrvOBJ.enableExtrapolation()
+    return [sofrIX, sfCrvOBJ, sfCrvHDL, sfParRT]      # 4つのオブジェクトを戻す
+  
+# TONAカーブ
+def makeTonaCurve(crvDATA):
+    '''makeTonaCurve(crvDATA)->[tonaIX,tnCrvOBJ,tnCrvHDL,tnParRT]'''
+  # 1.指標金利オブジェクト
+    tnCrvHDL = ql.RelinkableYieldTermStructureHandle()  
+    tonaIX = ql.OvernightIndex('TONA', Tp0,   jpyFX, calJP, dcA365, tnCrvHDL)
+  # 2. カーブヘルパー
+    cHelper, tnParRT = [], []
+    for knd, tnr, rt in crvDATA:
+      if knd == 'depo':
+          cHelper.append(ql.DepositRateHelper(sqHDL(rt/100),tonaIX)) 
+      if knd == 'swap':
+          cHelper.append(ql.OISRateHelper(Tp2, pD(tnr), sqHDL(rt/100),tonaIX))
+      tnParRT.append(rt/100)                                # パーレート用リスト
+  # カーブオブジェクト
+    tnCrvOBJ = ql.PiecewiseLogLinearDiscount(Tp0, calJP, cHelper, dcA365)
+    tnCrvHDL.linkTo(tnCrvOBJ) ; tnCrvOBJ.enableExtrapolation()
+    return [tonaIX, tnCrvOBJ, tnCrvHDL, tnParRT]        # 4つのオブジェクトを戻す    
+  
+# TIBORカーブ
+def makeTiborCurve(crvDATA):
+    '''makeTiborCurve(crvDATA)->[tbrIX,tbCrvOBJ,tbCrvHDL,tbParRT]'''
+  # 1.指標金利オブジェクト
+    tbCrvHDL = ql.RelinkableYieldTermStructureHandle() 
+    tbrIX    = ql.Tibor(pdFreqSA, tbCrvHDL)
+  # 2. HelperとTiborカーブオブジェクト
+    cHelper, tbParRT = [], []
+    for knd, tnr, rt in crvDATA:
+       if knd == 'depo': cHelper.append(ql.DepositRateHelper(sqHDL(rt/100),tbrIX)) 
+       if knd == 'swap': cHelper.append(ql.SwapRateHelper(   sqHDL(rt/100),
+                                     pD(tnr), calJP, freqSA, mFLLW, dcA365, tbrIX))
+       tbParRT.append(rt/100)                            # パーレート用リスト
+    tbCrvOBJ = ql.PiecewiseLogLinearDiscount(Tp2, calJP, cHelper, dcA365)
+    tbCrvHDL.linkTo(tbCrvOBJ) ; tbCrvOBJ.enableExtrapolation()
+    return [tbrIX, tbCrvOBJ, tbCrvHDL, tbParRT]  # 4つのオブジェクトを戻す  
+  
+# アニュイティ計算
+def calcAnnuity(annSCD, crvOBJ, dc=dcA365): 
+    '''calcAnnuity(annuitySCD, curveOBJ, dc=dcA365) -> Annuity)'''
+    discFCT = nA([crvOBJ.discount(xx) for xx in annSCD][1:])
+    tnrLST  = np.diff([dc.yearFraction(annSCD.startDate(), xx) for xx in annSCD]) 
+    return np.sum(tnrLST * discFCT) 
+
+##### Swap/債券/CDSキャッシュフロー表 #####
+# Swap
+def swapCashFlow(swapOBJ, curveOBJ, leg=1, dc=dcA365): 
+    '''swapCashFlow(swapOBJ, curveOBJ, leg=1:FLoat  0:Fix)-> DataFrame)''' 
+    settleDT = curveOBJ.referenceDate() 
+    if leg == 1:  # 変動ﾚｸﾞ leg(1)
+        dfSWP = pd.DataFrame({
+            'fixingDate': cpn.fixingDate().ISO(),
+            'accruStart': cpn.accrualStartDate(),                # No ISO form
+            'accruEnd':   cpn.accrualEndDate(),                  # No ISO form
+            'payDate':    cpn.date(),                            # No ISO form
+            'days':       dc.dayCount(cpn.accrualStartDate(),cpn.accrualEndDate()),
+            'rate':       cpn.rate(),
+            'spread':     cpn.spread(),
+            'amount':     cpn.amount(),
+            } for cpn in map(ql.as_floating_rate_coupon, swapOBJ.leg(1)))
+        # マルチカーブのフォワード                                  1000は便宜上の数字
+        fwdRT = [curveOBJ.forwardRate(                         
+                        dfSWP.accruStart[id],dfSWP.accruEnd[id], dcA365, cmpdSPL).rate()
+                      if settleDT < dfSWP.accruStart[id] else 1000 for id in dfSWP.index ] 
+        dfSWP = pd.concat([dfSWP, pd.DataFrame(fwdRT, columns=['fwdRT']) ], axis=1)
+        dfSWP.rate= dfSWP.rate.where(dfSWP.fwdRT>999, dfSWP.fwdRT)
+        dfSWP = dfSWP.drop('fwdRT', axis=1)
+        dfSWP.accruStart = dfSWP.accruStart.map(lambda x: x.ISO())
+        dfSWP.accruEnd = dfSWP.accruEnd.map(lambda x: x.ISO())
+    else:          # 固定ﾚｸﾞ leg(0)
+        dfSWP = pd.DataFrame({
+            'nominal':    cpn.nominal(),
+            'accruStart': cpn.accrualStartDate().ISO(),
+            'accruEnd':   cpn.accrualEndDate().ISO(),
+            'payDate':    cpn.date(),
+            'days':       dc.dayCount(cpn.accrualStartDate(),cpn.accrualEndDate()),
+            'rate':       cpn.rate(),
+            'amount':     cpn.amount()
+            } for cpn in map(ql.as_fixed_rate_coupon, swapOBJ.leg(0)))
+    # 起算日の挿入
+    dfEFF = pd.DataFrame([{'payDate': swapOBJ.startDate()}], columns=dfSWP.columns)
+    dfSWP = pd.concat([dfEFF, dfSWP], ignore_index=True)                
+    # ディスカウントファクター(DF)
+    psDF = [1.0                   for dt in dfSWP.payDate if dt <= settleDT] # past   DF
+    fuDF = [curveOBJ.discount(dt) for dt in dfSWP.payDate if settleDT < dt ] # future DF
+    dfSWP = pd.concat([dfSWP, pd.DataFrame(psDF+fuDF, columns=['DF']) ], axis=1)
+    dfSWP.payDate = dfSWP.payDate.map(lambda x: x.ISO())                        # ISOへ
+    return dfSWP
+
+# 債券 (past=0は過去キャッシュフローの非表示)
+def bondCashFlow(bondOBJ, ir='', yts='', past=0):    
+    '''1:(ir='',yts='')=No DF      2:(ir=irOBJ, yts='')=ir DF
+       3:(ir='', yts=ytOBJ)=yt DF  
+       4:( , ,past=0)=futureCF      5:( , ,past=1)=past+futureCF    '''
+    dfCPN = pd.DataFrame({
+        'payDate':    cpn.date(),          # no ISO
+        'coupon':     cpn.rate(),
+        'accruStart': cpn.accrualStartDate().ISO(),
+        'accruEnd':   cpn.accrualEndDate().ISO(),
+        'amount':     cpn.amount(),
+        } for cpn in map(ql.as_coupon, bondOBJ.cashflows()) if cpn is not None )
+    # 起算日, 元本
+    dfEFF = pd.DataFrame([{'payDate': bondOBJ.startDate()}], columns=dfCPN.columns)
+    dfPRN = pd.DataFrame({'payDate': cf.date(), 'amount':cf.amount()} for cf,cpn 
+                in zip(bondOBJ.cashflows(),map(ql.as_coupon, bondOBJ.cashflows())) 
+                                                                if cpn is None )
+    dfBND = pd.concat([dfEFF, dfCPN, dfPRN], ignore_index=True) 
+    # ディスカウントファクター列作成
+    settleDT = bondOBJ.settlementDate()
+    psDF = [1.0       for dt in dfBND.payDate if dt <= settleDT]     #past DF
+    # future DF
+    if ir != '' and yts == '' :                                     # irtOBJ
+      fuDF = [ir.discountFactor(settleDT, dt)
+                      for dt in dfBND.payDate if settleDT < dt ] 
+      dfBND = pd.concat([dfBND, pd.DataFrame(psDF+fuDF, columns=['DF']) ], axis=1)
+    elif yts != '' :                                                  # ytsOBJ
+      fuDF = [yts.discount(dt)
+                      for dt in dfBND.payDate if settleDT < dt ] 
+      dfBND = pd.concat([dfBND, pd.DataFrame(psDF+fuDF, columns=['DF']) ], axis=1)
+    # 将来キャッシュフローの抽出
+    if past == 0: 
+      dfBND = dfBND[dfBND.payDate >= settleDT]
+      dfBND = dfBND.reset_index(drop=True)              #インデックス番号リセット
+    dfBND.payDate  =  dfBND.payDate.map(lambda x: x.ISO())  # ISOフォーマットへ
+    return dfBND
+
+# CDS
+def cdsCashFlow(cdsOBJ, hzCvOBJ, dsCvOBJ):
+    '''cdsCashFlow(cdsOBJ, hzCvOBJ, dsCvOBJ)-> DataFrame)'''    
+    tradeDT, ntlAMT = cdsOBJ.tradeDate(), cdsOBJ.notional() 
+    dfCDS = pd.DataFrame({
+        'payDate':    cpn.date(),
+        'coupon':     cpn.rate(),
+        'accStt':     cpn.accrualStartDate().ISO(),
+        'accEnd':     cpn.accrualEndDate(),
+        'days':       cpn.amount()/(ntlAMT*cpn.rate()    /360),
+        'YF':         cpn.amount()/(ntlAMT*cpn.rate()*365/360),
+        'amount':     cpn.amount(),
+        } for cpn in map(ql.as_coupon, cdsOBJ.coupons()))
+    # 起算日の挿入
+    dfEFF = pd.DataFrame([{'payDate': cdsOBJ.protectionStartDate(), 
+                    'accEnd': cdsOBJ.protectionStartDate()}], columns=dfCDS.columns)
+    dfCDS = pd.concat([dfEFF, dfCDS], ignore_index=True)            
+    #ディスカウントファクター
+    psDF  = [1.0                  for dt in dfCDS.payDate if dt <= tradeDT] #past   DF
+    fuDF  = [dsCvOBJ.discount(dt) for dt in dfCDS.payDate if tradeDT < dt ] #future DF
+    dfCDS = pd.concat([dfCDS, pd.DataFrame(psDF+fuDF, columns=['DF']) ], axis=1)
+    #生存確率QとdQ
+    psQ   = [1.0                             for dt in dfCDS.accEnd if dt <= tradeDT]
+    fuQ   = [hzCvOBJ.survivalProbability(dt) for dt in dfCDS.accEnd if tradeDT < dt ]
+    dfCDS = pd.concat([dfCDS, pd.DataFrame(psQ+fuQ, columns=['Q']) ], axis=1) 
+    dQ    = np.insert(np.diff(dfCDS.Q)*(-1), 0, 0) 
+    dfCDS = pd.concat([dfCDS, pd.DataFrame( dict(dQ=dQ)) ], axis=1) 
+    # mid計算
+    midDTs  = dfCDS.payDate[:-1]+(np.diff(dfCDS.payDate)/2).astype('int')
+    midDFs  = [dsCvOBJ.discount(dd) for dd in midDTs]
+    midQs   = [hzCvOBJ.survivalProbability(dd) for dd in midDTs]
+    dfDFQm  = pd.DataFrame( dict(mDate=midDTs, mDF=midDFs, mQ=midQs))
+    dfDFQm.mDate = dfDFQm.mDate.map(lambda x: x.ISO())
+    dfDFQm  = pd.concat([pd.DataFrame(columns=['mDate'],index=[0]),     #空の行を追加
+                                                        dfDFQm],ignore_index=True)
+    dfCDS   = pd.concat([dfCDS, dfDFQm ], axis=1)  
+    #日付修正(ISOフォーマット)
+    dfCDS.payDate =  dfCDS.payDate.map(lambda x:x.ISO())
+    dfCDS.accEnd  =  dfCDS.accEnd.map(lambda x:x.ISO())
+    return dfCDS
+
+##### ノーマルモデル クラス #####
+class normalCalculator:
+    ''' Bachelier normal model class
+        normalCalculator(payOffObj, matDT, futRT, volRT, rfOBJ)    
+        matYR:満期日, SD:Standard Deviation,  DF:discFactor.'''
+
+    def __init__(self, payOffObj, matDT, futRT, volRT, rfOBJ):
+      # 満期年
+      self.trdDT_ = ql.Settings.instance().evaluationDate        
+      self.matYR_ = dcA365.yearFraction(self.trdDT_, matDT)
+      # ペイオフ
+      self.pC_    = payOffObj.optionType()
+      self.stkRT_ = payOffObj.strike()
+      # 満期日DF、カーブオブジェクト
+      self.matDF_ = rfOBJ.discount(matDT)
+      self.rfOBJ_ = rfOBJ
+      # ボラ、原資産価格
+      self.volRT_ = volRT
+      self.futRT_ = futRT
+
+    # SD: 標準偏差関数
+    def SD(self, volRT = None, matYR=None): 
+        volRT = volRT or self.volRT_ ; matYR = matYR or self.matYR_
+        return volRT*np.sqrt(matYR)
+    # dd: d1関数
+    def dd(self, futRT = None, volRT = None, matYR = None): 
+        futRT = futRT or self.futRT_ ; volRT = volRT or self.volRT_
+        matYR = matYR or self.matYR_
+        return self.pC_*(futRT - self.stkRT_)/self.SD(volRT, matYR)
+    # npvの計算  (関数内関数 : クロージャー)
+    def npv(self, futRT = None, volRT = None, matYR = None):
+        futRT = futRT or self.futRT_; volRT = volRT or self.volRT_
+        matYR = matYR or self.matYR_
+        matDF = self.matDF_ if matYR == self.matYR_ \
+                            else self.rfOBJ_.discount(matYR*360/365)        
+        d1 = self.dd(futRT, volRT, matYR) 
+        return matDF*self.SD(volRT,matYR)*(d1*norm.cdf(d1) + norm.pdf(d1))
